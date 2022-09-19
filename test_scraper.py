610b66bf9bc49f19b06a7ec3ae881df48ab0bd96
David Awoyelu <awoyelu123@gmail.com>
2022-09-19 17:31:19 +0100

    more test_scraper writting, moved data.json file

---
+++ test_scraper.py
@@ -0,0 +1,15 @@
+import unittest
+import water_scraper_mile_4
+
+class TestScraper(unittest.TestCase):
+    def test_link_list(self):
+        pass
+    def test_directory_production(self):
+        pass
+    def test_nav_to_crime_books(self):
+        result = 
+        self.assertEqual()
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
